d7d857eb8e7dc107ecf07da200db4b4c4ad95535
jianghangli@aengine.com.cn <jianghangli>
2022-09-13 16:53:25 +0800

    add tezos and templeWallet logos

--- src/utils/logos.ts
+++ src/utils/logos.ts
@@ -2,14 +2,18 @@ import KaikasLogo from '@/assets/kaikas-logo.png';
 import KlaytnLogo from '@/assets/klaytn-logo.png';
 import PhantomLogo from '@/assets/phantom-logo.png';
 import SolanaLogo from '@/assets/solana-logo.svg';
+import TempleLogo from '@/assets/temple-logo.png';
+import TezosLogo from '@/assets/tezos-logo.svg';
 import { ChainType, WalletType } from '@/wallets';
 
 export const ChainLogos: Record<ChainType, string> = {
   [ChainType.Solana]: SolanaLogo,
   [ChainType.Klaytn]: KlaytnLogo,
+  [ChainType.Tezos]: TezosLogo,
 };
 
 export const WalletLogos: Record<WalletType, string> = {
   [WalletType.Phantom]: PhantomLogo,
   [WalletType.Kaikas]: KaikasLogo,
+  [WalletType.Temple]: TempleLogo,
 };
