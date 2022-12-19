661e9918219303d8206bad2eae8594cacbab724d
tasse00 <tasse_00@163.com>
2022-12-19 10:44:16 +0800

    update: complete api url in canister

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,5 +1,7 @@
 export const PREFIX =
-  process.env.NODE_ENV === 'development' ? '/fcc-story' : '/fcc-story';
+  process.env.NODE_ENV === 'development'
+    ? 'https://findtruman.io/fcc-story'
+    : 'https://findtruman.io/fcc-story';
 
 export const LINK_STORY_GAMEPLAY = 'https://findtruman.io/co-creation/';
 export const LINK_ACHIEVEMENTS =
